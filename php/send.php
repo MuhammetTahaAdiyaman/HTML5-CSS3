25de1e7ac2ff5b1e4f513f9cd1e7e7d913403f37
Muhammet Taha Adıyaman <68828015+MuhammetTahaAdiyaman@users.noreply.github.com>
2022-06-03 17:17:08 +0300

    Add files via upload

---
+++ php/send.php
@@ -0,0 +1,8 @@
+<?php
+$userName=$_GET["userName"];
+$uName=$_GET["uName"];
+$userEmail=$_GET["userEmail"];
+echo "<h1>Formu send.php'ye form metodu attribute ile GET metodu ile gönderdiniz</h1>Kullanıcı Adınız: $userName <br>
+      Adınız: $uName <br>
+      E-posta Adresiniz: $userEmail";
+?>
---
+++ php/send2.php
@@ -0,0 +1,8 @@
+<?php
+$userName=$_POST["userName"];
+$uName=$_POST["uName"];
+$userEmail=$_POST["userEmail"];
+echo "<h1>butonu send2.php'ye butonda formmethod attirube ile post şekilde gönderdiniz</h1>Kullanıcı Adınız: $userName <br>
+      Adınız: $uName <br>
+      E-posta Adresiniz: $userEmail";
+?>
